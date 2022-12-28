9a72243bd25ef212342e1360b2a44f5a798bdb78
r.vinichenko <r.vinichenko@disoft.dev>
2022-12-28 10:34:56 +0200

    full js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
-$(".menu a, .hero__box a").on("click", function () {
+$('.menu a, .hero__box a').on('click', function () {
+  let href = $(this).attr('href');
 
-  let href = $(this).attr("href");
-
-  $("html, body").animate({
-    scrollTop: $(href).offset().top
-  }, {
-    duration: 500,
-    easing: "linear"
-  });
+  $('html, body').animate(
+    {
+      scrollTop: $(href).offset().top,
+    },
+    {
+      duration: 500,
+      easing: 'linear',
+    },
+  );
 
   return false;
 });
@@ -16,48 +18,48 @@ function ready() {
   let scrollPos = window.scrollY;
 
   if (scrollPos > 10) {
-    header.classList.add("header--active");
+    header.classList.add('header--active');
   } else {
-    header.classList.remove("header--active");
+    header.classList.remove('header--active');
   }
 }
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener('DOMContentLoaded', ready);
 
-let header = document.querySelector(".header");
-window.addEventListener("scroll", function () {
+let header = document.querySelector('.header');
+window.addEventListener('scroll', function () {
   let scrollPos = window.scrollY;
 
   if (header && scrollPos > 10) {
-    header.classList.add("header--active");
+    header.classList.add('header--active');
   } else {
-    header.classList.remove("header--active");
+    header.classList.remove('header--active');
   }
 });
 
-const burger = document.querySelector(".burger")
-const footer = document.querySelector(".footer")
-const burgerFooter = document.querySelector(".burger__footer")
-const menuMob = document.querySelector(".menu__mob")
-burger.addEventListener("click", function () {
-  menuMob.classList.toggle("menu__mob-active")
-  burger.classList.toggle("burger--active")
-  burgerFooter.classList.toggle("burger--active")
-  footer.classList.toggle("footer--inactive")
-})
-burgerFooter.addEventListener("click", function () {
-  menuMob.classList.toggle("menu__mob-active")
-  burger.classList.toggle("burger--active")
-  burgerFooter.classList.toggle("burger--active")
-  footer.classList.toggle("footer--inactive")
-})
+const burger = document.querySelector('.burger');
+const footer = document.querySelector('.footer');
+const burgerFooter = document.querySelector('.burger__footer');
+const menuMob = document.querySelector('.menu__mob');
+burger.addEventListener('click', function () {
+  menuMob.classList.toggle('menu__mob-active');
+  burger.classList.toggle('burger--active');
+  burgerFooter.classList.toggle('burger--active');
+  footer.classList.toggle('footer--inactive');
+});
+burgerFooter.addEventListener('click', function () {
+  menuMob.classList.toggle('menu__mob-active');
+  burger.classList.toggle('burger--active');
+  burgerFooter.classList.toggle('burger--active');
+  footer.classList.toggle('footer--inactive');
+});
 
-const linkin = document.querySelectorAll(".linkin")
+const linkin = document.querySelectorAll('.linkin');
 linkin.forEach(function (removingTel) {
   removingTel.addEventListener('click', function () {
-    menuMob.classList.remove("menu__mob-active")
-    burger.classList.remove("burger--active")
-    burgerFooter.classList.remove("burger--active")
-    footer.classList.remove("footer--inactive")
-  })
-})
+    menuMob.classList.remove('menu__mob-active');
+    burger.classList.remove('burger--active');
+    burgerFooter.classList.remove('burger--active');
+    footer.classList.remove('footer--inactive');
+  });
+});
